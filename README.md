c2e4931feea46d6176f425733179983aac0e1aaf
andersamer <sam.jon.andersen@gmail.com>
2024-05-08 22:46:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# barf-backup
+
+This is a script that I use to backup my [barf](https://git.btxx.org/barf/)-based blog to a git repo. You will have to initialized the directory to be backed up as a Git repo with a remote before using. 
+
+You can configure the script using a `.env` file with the following variables (All paths must be in quotes. *Make sure to use absolute paths!!*):
+
+* `TARGET_DIR` - The directory to be backed up.
+* `LOG_FILE` - A path at which to store the log file of the script.
+* `GIT_LOG_FILE` - A path at which to store the Git log file of the script.
+* `SUCCESS_MSG` - The message to display in the log upon a successful backup. I usually use `"Backup complete"`
+* `ERROR_MSG` - An error message to display upon backup failure. I usually use `"Error adding changes to git"`
+* `LOG_DATE` - How you want your log date formatted. I usually use `"$(date +'%Y-%m-%d')"`
---
+++ barf-backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# TODO: Find a way to decrease noise in logs
+
+# Load in the configuration variables from the .env file
+source .env
+
+# Navigate to the target directory
+cd $TARGET_DIR
+
+# Stage all changes and log the result
+if git add . >> "$GIT_LOG_FILE" 2>&1; then
+    echo "$LOG_DATE - Changes staged successfully" >> "$LOG_FILE"
+else
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - Error staging changes to git" >> "$LOG_FILE"
+    exit 1
+fi
+
+# Commit changes with a timestamp and log the result
+if git commit -m "Backup on $(date +'%Y-%m-%d %H:%M:%S')" >> "$GIT_LOG_FILE" 2>&1; then
+    echo "$LOG_DATE - Changes committed successfully" >> "$LOG_FILE"
+else
+    echo "$LOG_DATE - Error committing changes to git: no changes to commit" >> "$LOG_FILE"
+    exit 1
+fi
+
+# Push changes to the remote repository and log the result
+if git push origin main >> "$GIT_LOG_FILE" 2>&1; then
+    echo "$LOG_DATE - Changes pushed successfully" >> "$LOG_FILE"
+else
+    echo "$LOG_DATE - Error pushing changes to git" >> "$LOG_FILE"
+    exit 1
+fi
+
+# Exit with success status
+exit 0
